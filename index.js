05b6bcb70cc3951c04915c952cd83fccd887c3e7
AJ Jalusi <ajalusi26@gmail.com>
2022-01-04 11:42:52 -0500

    added script link

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function getData(){
+    fetch(url)
+    .then((r) => r.json())
+    .then(data => updatePrompt(data))
+}
+function updatePrompt(data){
+    let num = Math.floor(Math.random() * 10)
+    let propmt = document.querySelector('h3')
+    propmt.innerText = data[num].propmt
+}

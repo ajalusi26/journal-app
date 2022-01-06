e56e5d2b31b4f8d214322ffbaf76219631434fa2
AJ Jalusi <ajalusi26@gmail.com>
2022-01-06 16:15:47 -0500

    js all good, css a mess :D

--- index.js
+++ index.js
@@ -1,226 +1,229 @@
 const promptUrl = "http://localhost:3000/prompts"
 const journalUrl = "http://localhost:3000/entryInfo"
 
-//gets todays date
 const today = new Date().toLocaleDateString()
-let date = document.querySelector('.date')
-date.innerText = `Today is ${today}`
+    let date = document.querySelector('.date')
+    date.innerText = `Today is ${today}`
 
 
-//main functions
-function getJournalData() {
+
+function getJournalData(){
     fetch(journalUrl)
-        .then((r) => r.json())
-        .then(data => postJournalDataFromDB(data))
+    .then((r) => r.json())
+    .then(data => postJournalDataFromDB(data))
 }
-function getPromptData() {
+function getPromptData(){
     fetch(promptUrl)
-        .then((r) => r.json())
-        .then(data => updatePrompt(data))
+    .then((r) => r.json())
+    .then(data => updatePrompt(data))
 }
-function submitJournal() {
-    const form = document.getElementById('form')
+function submitJournal(){
+    const form = document.getElementById('inputForm')
     form.addEventListener('submit', e => {
         e.preventDefault()
         let journalText = e.target['input'].value
         let prompt = document.querySelector('h3').innerText
-        let submitDate = today
+        let submitDate = today 
         let newJournalData = {
             journalText: journalText,
             prompt: prompt,
             date: submitDate
         }
         sendToDB(newJournalData)
-
+        
         form.reset()
-
+        
     })
 }
-function dateSelection() {
+function dateSelection(){
     const selector = document.querySelector('.months')
     let entryList = document.querySelectorAll('.divPractice')
     selector.addEventListener('click', e => {
         switch (e.target.value) {
-            case 'all':
+            case 'all': 
                 entryList = document.querySelectorAll('.divPractice')
                 entryList.forEach(item => {
                     item.style.display = "block"
                 });
-                break;
-            case 'january':
+                    break;
+            case 'january': 
                 entryList = document.querySelectorAll('.divPractice')
                 entryList.forEach(item => {
-                    if (item.innerText.substring(0, 1).includes(1, "/")) {
-                        item.style.display = "block"
-                    } else {
-                        item.style.display = "none"
-                    }
+                     if (item.innerText.substring(0,2) === '1/'){
+                         item.style.display = "block"
+                     }else{
+                         item.style.display = "none"
+                     }
+                });
+                    break;
+                case 'february': 
+                entryList = document.querySelectorAll('.divPractice')
+                entryList.forEach(item => {
+                     if (item.innerText.substring(0,2) === '2/'){
+                         item.style.display = "block"
+                     }else{
+                         item.style.display = "none"
+                     }
+                     
                 });
                 break;
-            case 'february':
+                case 'march': 
                 entryList = document.querySelectorAll('.divPractice')
                 entryList.forEach(item => {
-                    if (item.innerText.substring(0, 1).includes(2, "/")) {
-                        item.style.display = "block"
-                    } else {
-                        item.style.display = "none"
-                    }
-
+                     if (item.innerText.substring(0,2) === '3/'){
+                         item.style.display = "block"
+                     }else{
+                         item.style.display = "none"
+                     }
+                     
                 });
                 break;
-            case 'march':
+                case 'april': 
                 entryList = document.querySelectorAll('.divPractice')
                 entryList.forEach(item => {
-                    if (item.innerText.substring(0, 1).includes(3, "/")) {
-                        item.style.display = "block"
-                    } else {
-                        item.style.display = "none"
-                    }
-
+                     if (item.innerText.substring(0,2) === '4/'){
+                         item.style.display = "block"
+                     }else{
+                         item.style.display = "none"
+                     }
+                     
                 });
                 break;
-            case 'april':
+                case 'may': 
                 entryList = document.querySelectorAll('.divPractice')
                 entryList.forEach(item => {
-                    if (item.innerText.substring(0, 1).includes(4, "/")) {
-                        item.style.display = "block"
-                    } else {
-                        item.style.display = "none"
-                    }
-
+                     if (item.innerText.substring(0,2) === '5/'){
+                         item.style.display = "block"
+                     }else{
+                         item.style.display = "none"
+                     }
+                     
                 });
                 break;
-            case 'may':
+                case 'june': 
                 entryList = document.querySelectorAll('.divPractice')
                 entryList.forEach(item => {
-                    if (item.innerText.substring(0, 1).includes(5, "/")) {
-                        item.style.display = "block"
-                    } else {
-                        item.style.display = "none"
-                    }
-
+                     if (item.innerText.substring(0,2) === '6/'){
+                         item.style.display = "block"
+                     }else{
+                         item.style.display = "none"
+                     }
+                     
                 });
                 break;
-            case 'june':
+                case 'july': 
                 entryList = document.querySelectorAll('.divPractice')
                 entryList.forEach(item => {
-                    if (item.innerText.substring(0, 1).includes(6, "/")) {
-                        item.style.display = "block"
-                    } else {
-                        item.style.display = "none"
-                    }
-
+                     if (item.innerText.substring(0,2) === '7/'){
+                         item.style.display = "block"
+                     }else{
+                         item.style.display = "none"
+                     }
+                     
                 });
                 break;
-            case 'july':
+                case 'august': 
                 entryList = document.querySelectorAll('.divPractice')
                 entryList.forEach(item => {
-                    if (item.innerText.substring(0, 1).includes(7, "/")) {
-                        item.style.display = "block"
-                    } else {
-                        item.style.display = "none"
-                    }
-
+                     if (item.innerText.substring(0,2) === '8/'){
+                         item.style.display = "block"
+                     }else{
+                         item.style.display = "none"
+                     }
+                     
                 });
                 break;
-            case 'august':
+                case 'september': 
                 entryList = document.querySelectorAll('.divPractice')
                 entryList.forEach(item => {
-                    if (item.innerText.substring(0, 1).includes(8, "/")) {
-                        item.style.display = "block"
-                    } else {
-                        item.style.display = "none"
-                    }
-
+                     if (item.innerText.substring(0,2) === '9/'){
+                         item.style.display = "block"
+                     }else{
+                         item.style.display = "none"
+                     }
+                     
                 });
                 break;
-            case 'september':
+                case 'october': 
                 entryList = document.querySelectorAll('.divPractice')
                 entryList.forEach(item => {
-                    if (item.innerText.substring(0, 1).includes(9, "/")) {
-                        item.style.display = "block"
-                    } else {
-                        item.style.display = "none"
-                    }
-
+                     if (item.innerText.substring(0,2) === '10'){
+                         item.style.display = "block"
+                     }else{
+                         item.style.display = "none"
+                     }
+                     
                 });
                 break;
-            case 'october':
+                case 'november': 
                 entryList = document.querySelectorAll('.divPractice')
                 entryList.forEach(item => {
-                    if (item.innerText.substring(0, 1).includes(1, 0)) {
-                        item.style.display = "block"
-                    } else {
-                        item.style.display = "none"
-                    }
-
+                     if (item.innerText.substring(0,2) === '11'){
+                         item.style.display = "block"
+                     }else{
+                         item.style.display = "none"
+                     }
+                     
                 });
                 break;
-            case 'november':
+                case 'december': 
                 entryList = document.querySelectorAll('.divPractice')
                 entryList.forEach(item => {
-                    if (item.innerText.substring(0, 1).includes(1, 1)) {
-                        item.style.display = "block"
-                    } else {
-                        item.style.display = "none"
-                    }
-
-                });
-                break;
-            case 'december':
-                entryList = document.querySelectorAll('.divPractice')
-                entryList.forEach(item => {
-                    if (item.innerText.substring(0, 1).includes(1, 2)) {
-                        item.style.display = "block"
-                    } else {
-                        item.style.display = "none"
-                    }
-
+                     if (item.innerText.substring(0,2) === '12'){
+                         item.style.display = "block"
+                     }else{
+                         item.style.display = "none"
+                     }
+                     
                 });
                 break;
         }
 
     })
-
+    
 }
 
-//callback functions
 
-function updatePrompt(data) {
+
+
+function updatePrompt(data){
     let num = Math.floor(Math.random() * 10)
     let updatePrompt = document.querySelector('h3')
     updatePrompt.innerText = data[num].prompt
 }
-function sendToDB(newJournalData) {
+function sendToDB(newJournalData){
     fetch(journalUrl, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newJournalData),
-    })
-        .then((r) => r.json())
-        .then((obj) => addOneEntry(obj));
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(newJournalData),
+})
+  .then((r) => r.json())
+  .then((obj) => addOneEntry(obj));
 }
-function addOneEntry(obj) {
-    let container = document.querySelector('#divContainer')
-    let outerUl = document.createElement('ul')
-    let div = document.createElement('div')
-    let innerUl = document.createElement('p')
-    let button = document.createElement('button')
-    button.innerText = "Delete"
-    div.classList.add('divPractice')
-    div.id = obj.id
-    innerUl.id = obj.id
-    innerUl.innerText = obj.date
-    outerUl.appendChild(div)
-    div.appendChild(innerUl)
-    innerUl.appendChild(button)
-    container.appendChild(outerUl)
-    console.log(outerUl)
-    popupText(div)
+function addOneEntry(obj){
+        let container = document.querySelector('#divContainer')
+        let outerUl = document.createElement('ul')
+        let div = document.createElement('div')
+        let innerUl = document.createElement('p')
+        let button = document.createElement('button')
+        button.innerText = "Delete"
+        button.id = obj.id
+        outerUl.classList.add('ulEntries')
+        outerUl.id = obj.id
+        div.id = obj.id
+        innerUl.id = obj.id
+        innerUl.innerText = obj.date
+        outerUl.appendChild(div)
+        div.appendChild(innerUl)
+        outerUl.appendChild(button)
+        container.appendChild(outerUl)
+        console.log(outerUl)
+        deleteButton(button)
+        popupText(outerUl, button)
 }
-function postJournalDataFromDB(data) {
+function postJournalDataFromDB(data){
     data.forEach(item => {
         let container = document.querySelector('#divContainer')
         let outerUl = document.createElement('ul')
@@ -228,51 +231,118 @@ function postJournalDataFromDB(data) {
         let innerUl = document.createElement('p')
         let button = document.createElement('button')
         button.innerText = "Delete"
-        div.classList.add('divPractice')
+        button.id = item.id
+        outerUl.classList.add('ulEntries')
+        outerUl.id = item.id
         div.id = item.id
         innerUl.id = item.id
         innerUl.innerText = item.date
-        innerUl.appendChild(button)
         outerUl.appendChild(div)
+        outerUl.append(button)
         div.appendChild(innerUl)
         container.appendChild(outerUl)
-        console.log(container)
-        popupText(div)
+        deleteButton(button)
+        popupText(outerUl, button)
     });
 }
-function popupText(div) {
-    div.addEventListener('click', e => {
+function popupText(div, button){
+    div.addEventListener('click', e =>{
+        if(e.target == button) return;
         let blurOne = document.querySelector("#divContainer");
         blurOne.classList.add("active");
         let blurTwo = document.querySelector("#divInput");
         blurTwo.classList.add("active");
         let popup = document.getElementById("popup");
         popup.classList.add("active");
-
+        
         // let button = document.createElement('button')
         // button.classList.add('close-popup')
         // button.innerText = "Close"
         // popup.append(button)
-
+        
         //let text = document.getElementsByClassName('popup-text')
-        fetch(`${journalUrl}/${e.target.id}`)
-            .then((r) => r.json())
-            .then(data => {
-                let popupHTML = `<h2>${data.prompt}</h2><p>${data.journalText}</p>`
-                popup.innerHTML = popupHTML
-                console.log(popup)
-            })
-        popup.addEventListener('click', e => {
-            let blurOne = document.querySelector("#divContainer");
-            blurOne.classList.remove("active");
-            let blurTwo = document.querySelector("#divInput");
-            blurTwo.classList.remove("active");
-            let popup = document.getElementById("popup");
-            popup.classList.remove("active");
+        fetch(`${journalUrl}/${e.target.id}`) 
+        .then((r) => r.json())
+        .then(data => {
+                let popupHTML = `<h2 id = ${data.id}>${data.prompt}</h2><p>${data.journalText}</p>`
+                popup.innerHTML =  popupHTML
+        
+                let popupData = {
+                    date: data.date ,
+                    journalText: data.journalText,
+                    prompt: data.prompt
+                }
+                let id = data.id
+                console.log(popupData, 'popup data')
+                console.log(data, 'data')
+                comment(popup, popupData, id, data)
         })
     })
-
+    
 }
+function deleteButton(button){
+    button.addEventListener('click', e => {
+        let parent = button.parentNode
+        parent.style.display = "none"
+        fetch( `${journalUrl}/${button.id}`, {
+        method: "DELETE",
+        })
+        .then((r) => r.json())
+        .then(() => console.log("Deleted"));
+            })
+}
+function comment(popup, popupData, id, data){
+    let p = document.createElement('p')
+    let commentForm = document.createElement('form')
+    let input = document.createElement('input')
+    let submitBttn = document.createElement('input')
+    let closeBttn = document.createElement('button')
+    let commentP = document.createElement('p')
+    let commentTitle = document.createElement('p')
+    commentTitle.textContent = "Your Comment:"
+    commentP.textContent = data.comment
+    input.type = "text"
+    input.name = "comment"
+    submitBttn.type = "submit"
+    submitBttn.value = "Submit"
+    p.innerText = "Submit New Comment"
+    closeBttn.innerText = "Close"
+    commentForm.append(input, submitBttn)
+    popup.append(p)
+    popup.append(commentForm)
+    
+    commentForm.addEventListener('submit', e => {
+        e.preventDefault()
+        let comment = 'comment'
+        popupData[comment] =  e.target['comment'].value
+        commentP.textContent = e.target["comment"].value
+        fetch(`${journalUrl}/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(popupData),
+        })
+        .then((r) => r.json())
+        .then((obj) => console.log(obj));
+        commentForm.reset()
+        
+    })
+
+    closeBttn.addEventListener('click', e => {
+        let blurOne = document.querySelector("#divContainer");
+        blurOne.classList.remove("active");
+        let blurTwo = document.querySelector("#divInput");
+        blurTwo.classList.remove("active");
+        let popup = document.getElementById("popup");
+        popup.classList.remove("active");
+    })
+    commentTitle.appendChild(commentP)
+    popup.append(commentTitle)
+    
+    popup.append(closeBttn)
+}
+
 
 dateSelection()
 getJournalData()
